85f93e97f73282881ed7ac9a771b4a4a998774c8
dbeard <dbeard@groupon.com>
2017-04-01 18:06:13 -0700

    Solve problem 22

---
+++ Problems/p22.hs
@@ -0,0 +1,24 @@
+-- Using names.txt (right click and 'Save Link/Target As...'), a 46K text file containing over five-thousand first names, begin by sorting it into alphabetical order. Then working out the alphabetical value for each name, multiply this value by its alphabetical position in the list to obtain a name score.
+
+-- For example, when the list is sorted into alphabetical order, COLIN, which is worth 3 + 15 + 12 + 9 + 14 = 53, is the 938th name in the list. So, COLIN would obtain a score of 938 × 53 = 49714.
+
+-- What is the total of all the name scores in the file?
+
+import System.IO
+import Data.List
+import Data.Char
+
+nameScore :: String -> Int
+nameScore name = sum $ map (\chr -> (ord chr)-64) name
+
+calc :: [String] -> Int 
+calc input = do 
+    let nameScores = map (\x -> nameScore x) $ sort input
+        multipliedNameScores = [x*(nameScores !! (x-1)) | x <- [1..(length nameScores)]] -- element * index
+    sum multipliedNameScores
+
+main :: IO ()
+main = do
+    withFile "Resources/p22.txt" ReadMode (\handle -> do
+        contents <- hGetContents handle 
+        putStr $ show $ calc $ lines contents)
