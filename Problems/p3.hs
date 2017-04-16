14373ccec57b6d50bea8e831ce63ba14e3af4d22
dbeard <dbeard@groupon.com>
2017-04-16 14:53:58 -0700

    Solve problem 12

--- Problems/p3.hs
+++ Problems/p3.hs
@@ -1,14 +1,13 @@
 -- The prime factors of 13195 are 5, 7, 13 and 29.
 -- What is the largest prime factor of the number 600851475143 ?
 
-import Utils.Primes
 import Data.List
 
 value :: Int
 value = 600851475143
 
 sqrt' :: Int -> Int
-sqrt' n = ceiling $ sqrt(fromIntegral n) 
+sqrt' n = ceiling $ sqrt(fromIntegral(n)) 
 
 firstFactor :: Int -> [Int]
 firstFactor n = take 1 $ filter (\x -> (n `rem` x == 0)) [2..sqrt'(n-1)]
